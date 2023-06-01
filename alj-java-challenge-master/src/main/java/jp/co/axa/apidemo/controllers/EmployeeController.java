7ee907ebd3f10f449e55c65f7c5258375269e2f2
sin-mohit <mohit.singh.th@his-world.com>
2023-06-01 20:37:46 +0900

    add extracted files

---
+++ alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -0,0 +1,54 @@
+package jp.co.axa.apidemo.controllers;
+
+import jp.co.axa.apidemo.entities.Employee;
+import jp.co.axa.apidemo.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    public void setEmployeeService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        List<Employee> employees = employeeService.retrieveEmployees();
+        return employees;
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
+        return employeeService.getEmployee(employeeId);
+    }
+
+    @PostMapping("/employees")
+    public void saveEmployee(Employee employee){
+        employeeService.saveEmployee(employee);
+        System.out.println("Employee Saved Successfully");
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        System.out.println("Employee Deleted Successfully");
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    public void updateEmployee(@RequestBody Employee employee,
+                               @PathVariable(name="employeeId")Long employeeId){
+        Employee emp = employeeService.getEmployee(employeeId);
+        if(emp != null){
+            employeeService.updateEmployee(employee);
+        }
+
+    }
+
+}
